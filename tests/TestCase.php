d007137b4ab9f524f366bd61fc44264c96019229
m1stfalre <misha.12345389@gmail.com>
2026-05-23 23:48:03 +0500

    fixed tests database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use DatabaseMigrations;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Создаем тестовые данные если таблица пуста
+        if (\App\Models\CreativityType::count() === 0) {
+            \App\Models\CreativityType::create([
+                'name' => 'Test Type',
+                'description' => 'Test Description'
+            ]);
+        }
+    }
 }
